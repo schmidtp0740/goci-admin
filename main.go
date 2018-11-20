e991d83c1f7b2bc2b44e8c793c400d0f4c0e2348
schmidtp0740 <schmidtp0740@gmail.com>
2018-11-20 10:20:12 -0600

    added docs for app

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+// gociadm - scans an oci env and checks for running instances.
+// If it determines that those instances are running then it will
+// stop them after a certain time. It will not include instances
+// that are in the OKE node pool.
 func main() {
 	// every 5 seconds scan for instances in compartment
 	scanOciEnv()
